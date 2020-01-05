AOSiP/device_aosip_sepolicy

sepolicy: update qcom/sepolicy.mk
Change-Id: Ib30abbc335657e72b5faafefe86db4dfe6c2c53e
Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

qcom/sepolicy.mk
@@ -3,5 +3,20 @@
 # that inherit from AOSiP
 #
 
+ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
+endif
+endif
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/aosip/sepolicy/qcom/private
+
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/aosip/sepolicy/qcom/dynamic
+else
 BOARD_SEPOLICY_DIRS += \
+    device/aosip/sepolicy/qcom/dynamic \
     device/aosip/sepolicy/qcom/vendor
+endif

bhi768/device_candy_sepolicy

qcom/sepolicy.mk
@@ -2,10 +2,21 @@
 # This policy configuration will be used by all qcom products
 # that inherit from Lineage
 #
+ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
+endif
+endif
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/candy/sepolicy/qcom/private
 
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/candy/sepolicy/qcom/dynamic
+else
 BOARD_SEPOLICY_DIRS += \
+    device/candy/sepolicy/qcom/dynamic \
     device/candy/sepolicy/qcom/vendor \
     device/candy/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
+endif
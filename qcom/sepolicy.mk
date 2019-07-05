a3b8b07da6c7892c08eb4bb660bdd14de2eeae74
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2019-07-05 11:25:12 -0400

    sepolicy: update qcom/sepolicy.mk
    Change-Id: Ib30abbc335657e72b5faafefe86db4dfe6c2c53e
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -2,10 +2,21 @@
 # This policy configuration will be used by all qcom products
 # that inherit from Lineage
 #
+ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
+endif
+endif
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/candy/sepolicy/qcom/private
 
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/candy/sepolicy/qcom/dynamic
+else
 BOARD_SEPOLICY_DIRS += \
+    device/candy/sepolicy/qcom/dynamic \
     device/candy/sepolicy/qcom/vendor \
     device/candy/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
+endif
